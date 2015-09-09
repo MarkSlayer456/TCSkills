13c4fe7451ab18299460c0bf175b5fbbdd24023c
BlueMoony <bjorn.huysmans@gmail.com>
2015-09-09 21:16:13 +0200

    Added class functionallity

--- TCSkills/src/eu/tamrielcraft/TCSkills/classes/Knight.java
+++ TCSkills/src/eu/tamrielcraft/TCSkills/classes/Knight.java
@@ -11,4 +11,19 @@ public class Knight extends Classes{
     public static Knight getInstance() {
             return instance;
     }
+    
+    @Override
+	public String className() {
+		return "Knight";
+	}
+
+	@Override
+	public String classNameChat() {
+		return "[" + className() + "]";
+	}
+
+	@Override
+	public String getDetails() {
+		return "A knight is a class that is superior in face-to-face combat and is specialized in heavy armor";
+	}
 }
